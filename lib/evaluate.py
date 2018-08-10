1d4aa3ab7171f3c8ce18e28bf08ffd7187a45321
Jiaxuan Wang <jiaxuan@umich.edu>
2018-08-10 10:51:47 -0500

    evaluation code updated

--- lib/evaluate.py
+++ lib/evaluate.py
@@ -5,13 +5,19 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
+def plot_fill(x, lines, color='b', label='default'):
+    for l in lines:
+        plt.plot(x, l, color=color, alpha=0.2)
+    plt.plot(x, np.array(lines).mean(0), color=color, label=label)
+    
 def getTheta(model, x):
     xs = model.localization(x)
     xs = xs.view(-1, model.num_flat_features(xs))
     theta = model.fc_loc(xs).cpu().data.numpy()
     return theta
     
-def visualize_stn_ts(visual_loader, model, feature=0, top=3, use_cuda=True):
+def visualize_stn_ts(visual_loader, model, feature=0, top=3, use_cuda=True,
+                     ylabel='magnitude', fontsize=15):
     # Get a batch of training data
     data, _ = next(iter(visual_loader))
     data = Variable(data, volatile=True)
@@ -21,21 +27,36 @@ def visualize_stn_ts(visual_loader, model, feature=0, top=3, use_cuda=True):
     
     input_tensor = data.cpu().data.squeeze(1).numpy()
     transformed_input_tensor = model.stn(data).cpu().data.squeeze(1).numpy()
+    print("different?", np.abs(input_tensor - transformed_input_tensor).sum())
     
     n, t, d = input_tensor.shape
-        
-    for i in range(min(n,top)):
-        plt.subplot(121)
+
+    nrows = min(n,top)
+    fig, axes = plt.subplots(nrows=nrows, ncols=2)
+    fig.tight_layout()
+    print(getTheta(model, data)[:nrows,:])    
+    for i in range(nrows):
+        a = transformed_input_tensor[i,:,feature]
+        b = input_tensor[i,:,feature]
+        m, M = min(min(a), min(b)), max(max(a), max(b))
+
+        plt.subplot("%d2%d" % (nrows, i*2+1))
         plt.plot(input_tensor[i,:,feature])
-        ax = plt.gca()
-        ylim = ax.get_ylim()
+        # ax = plt.gca()
+        # ylim = ax.get_ylim()
+        ylim = [m-0.01, M+0.01]
         plt.ylim(ylim)
-        plt.title('original')
-        plt.subplot(122)
+        plt.title('original', fontsize=fontsize)
+        plt.xlabel('time', fontsize=fontsize)
+        plt.ylabel(ylabel, fontsize=fontsize)
+        plt.grid(linestyle='--')        
+
+        plt.subplot("%d2%d" % (nrows, i*2+2))        
         plt.plot(transformed_input_tensor[i,:,feature])
         plt.ylim(ylim)
-        plt.title('transformed')
-        plt.show()
+        plt.title('transformed', fontsize=fontsize)
+        plt.xlabel('time', fontsize=fontsize)
+        plt.grid(linestyle='--')
 
 def theta_cluster(loader, model, use_cuda=True):
     theta = []
